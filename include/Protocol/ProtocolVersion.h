f65aed3da2458e6b7eb610bcc7a6cc73dfde5f66
niche0905 <yudle537@gmail.com>
2026-03-16 22:30:56 +0900

    Protocol Version Add

---
+++ include/Protocol/ProtocolVersion.h
@@ -0,0 +1,9 @@
+﻿#pragma once
+
+#include <cstdint>
+
+namespace se::protocol
+{
+    constexpr uint32_t kProtocolMagic = 0x53455256;     // 'SERV' in ASCII
+    constexpr uint32_t kProtocolVersion = 1;
+}   
